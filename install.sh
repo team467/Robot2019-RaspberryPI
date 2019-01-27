f474294fbdbae1ed32e40186acbc0954cb51162c
Andrew Friedman <arfrie22@students.shrewsbury.k12.ma.us>
2019-01-27 13:28:55 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo su pi
 sudo apt-get install git -y
+git clone -b programs-added https://github.com/team467/Robot2019-RaspberryPI.git
+cd Robot2019-RaspberryPI
 sudo mv * ../
 cd ../
 cd scripts
