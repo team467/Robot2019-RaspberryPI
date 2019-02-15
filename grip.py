517ad35b201ea0826585e75368ef75b51f0b614f
Akkuma22 <akkuma22@students.shrewsbury.k12.ma.us>
2019-02-14 19:35:49 -0500

    Update grip.py

--- grip.py
+++ grip.py
@@ -13,8 +13,8 @@ class TapeRecognitionCode:
         """
 
         self.__cv_resize_dsize = (0, 0)
-        self.__cv_resize_fx = 0.2
-        self.__cv_resize_fy = 0.2
+        self.__cv_resize_fx = 0.875
+        self.__cv_resize_fy = 0.875
         self.__cv_resize_interpolation = cv2.INTER_LINEAR
 
         self.cv_resize_output = None
