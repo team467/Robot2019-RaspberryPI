a01f5fd8d7feac6ae498a5edbde5364d0beeeb28
Akkuma22 <akkuma22@students.shrewsbury.k12.ma.us>
2019-02-16 15:54:12 -0500

    detecting firing range

--- opencv_AngleFinder_pi.py
+++ opencv_AngleFinder_pi.py
@@ -18,6 +18,7 @@ def extra_processing(pipeline):
     heights = []
     angle = 0
     frame_width_midpt = 80
+    distanceFromTarget = 0
     # Find the bounding boxes of the contours to get x, y, width, and height
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
@@ -30,8 +31,21 @@ def extra_processing(pipeline):
 
     if len(heights) == 2:
 
-        midpoint = ((center_x_positions[0] + center_x_positions[1])/2)
+        if widths[0] > widths[1]:
+            biggerWidth = widths[0]
+        else:
+            biggerWidth = widths[1]
+
+        midpoint = ((center_x_positions[0] + center_x_positions[1])/2.0)
         distance = abs(midpoint - (frame_width_midpt * pipeline._TapeRecognitionCode__cv_resize_fx))
+
+        averageWidth = (widths[0] + widths[1])/2.0
+
+        #Distance from the reflective tape in inches
+        if  abs(widths[0] - widths[1]) <= (0.3 * biggerWidth):
+            distanceFromTarget = 12.0 * (4.6 - ((2.0/15.0) * averageWidth))
+        else:
+            distanceFromTarget = sys.maxint
 
         angle = 35 * distance / (frame_width_midpt * pipeline._TapeRecognitionCode__cv_resize_fx)
 
@@ -42,14 +56,18 @@ def extra_processing(pipeline):
         angle = sys.maxint
         have_angle = False
     
-    return angle, have_angle
+    return angle, have_angle, distanceFromTarget 
 
 def main():
 
     turning_angle = 0 
     haveAngle = False
-
+    distance_from_target = 0
     frame_print = 1
+    firing_range_hatch = False
+    firing_range_cargo = False
+    cargoRange = 24
+    hatchRange = 24
 
     cond = threading.Condition()
     notified = [False]
@@ -95,14 +113,25 @@ def main():
             
             pipeline.process(frame)
             if pipeline is not None:
-                turning_angle, haveAngle = extra_processing(pipeline)
+                turning_angle, haveAngle, distance_from_target = extra_processing(pipeline)
 
                 table.putBoolean('haveAngle', haveAngle)
                 table.putNumber('angle', turning_angle)
 
-        
-        
-        
+                if distance_from_target < hatchRange:
+                    firing_range_hatch = True
+                    table.putBoolean('FiringRange_Hatch', firing_range_hatch)
+                else:
+                    firing_range_hatch = False
+                    table.putBoolean('FiringRange_Hatch', firing_range_hatch)
+
+                if distance_from_target < cargoRange:
+                    firing_range_cargo = True
+                    table.putBoolean('FiringRange_Cargo', firing_range_cargo)
+                else:
+                    firing_range_cargo = False
+                    table.putBoolean('FiringRange_Cargo', firing_range_cargo)
+
     cap.release()
 
 if __name__ == '__main__':
