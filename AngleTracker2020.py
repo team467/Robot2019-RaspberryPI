24181bc1a5b4f59eccfe471d223d2a12939bc79d
Akkuma22 <akkuma22@students.shrewsbury.k12.ma.us>
2020-01-11 18:04:20 -0500

    retrained grip, bounding box drawn
    Now using grip_three_convexhull.py in AngleTracker2020.py and AngleTracker2020.py is consistently drawing a bounding rectangle around target.

--- AngleTracker2020.py
+++ AngleTracker2020.py
@@ -1,10 +1,10 @@
 import cv2
-
 #from networktables import NetworkTables
 from grip_two import TapeRecCodeTwo
+from grip_three_convexhull import TapeRecCodeThree
 
 
-def extra_processing(pipeline2, frame):
+def extra_processing(pipeline3, frame):
     """
     Performs extra processing on the pipeline's outputs and publishes data to NetworkTables.
     :param pipeline: the pipeline that just processed an image
@@ -14,30 +14,53 @@ def extra_processing(pipeline2, frame):
     center_y_positions = []
     widths = []
     heights = []
+    x = 0
+    y = 0
+    w = 0
+    h = 0
  
     # Find the bounding boxes of the contours to get x, y, width, and height
-    print(len(pipeline2.filter_contours_output))
-    for contour in pipeline2.filter_contours_output:
+    # print(len(pipeline3.filter_contours_output))
+
+    for contour in pipeline3.filter_contours_output:
+
         x, y, w, h = cv2.boundingRect(contour)
-        if w > 80 and h > 80:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
+
+        # cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
 
         center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
         center_y_positions.append(y + h / 2)
         widths.append(w)
         heights.append(h)
+    
+        if float(w/h) >= 2:
+            if float(w/h) <= 2.3:
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
+                cv2.line(frame, (x,y), (x,y), (0,255,0), 10)
+                cv2.imshow("frame", frame) 
+                print("h = {}, w = {}, x = {}, y = {}, a = {}".format(h, w, x, y, (w*h)))
+
+    # if len(pipeline3.filter_contours_output) > 1:
+        # cv2.imshow("frame", frame)
+
+    # if len(pipeline3.filter_contours_output) == 1:
+    #     cv2.imshow("frame", frame)
+
+    # if (len(widths) == 1) and (len(heights) == 1):
+        # do angle calculations here
+
         
-    # print(pipeline2.filter_contours_output)
 
 def main():
     pipeline2 = TapeRecCodeTwo()
+    pipeline3 = TapeRecCodeThree()
     cap = cv2.VideoCapture(1)
     while True:
         have_frame, frame = cap.read()
         if have_frame:
-            pipeline2.process(frame)
-            extra_processing(pipeline2, frame)
-            cv2.imshow("frame", frame)
+            pipeline3.process(frame)
+            extra_processing(pipeline3, frame)
+            # cv2.imshow("frame", frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
