72bdd55ee398bba239d60c1a45bbffff17b5f38a
Akkuma22 <akkuma22@students.shrewsbury.k12.ma.us>
2020-02-24 18:53:11 -0500

    LDPreloadVision2020_multiCamera.sh change
    the script now sets the camera settings before running the tracker

--- LDPreloadVision2020_multiCamera.sh
+++ LDPreloadVision2020_multiCamera.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+v4l2-ctl -c brightness=60
+v4l2-ctl -c exposure_auto=1
+v4l2-ctl -c exposure_absolute=5
+
+v4l2-ctl -d 2 -c brightness=60
+v4l2-ctl -d 2 -c exposure_auto=1
+v4l2-ctl -d 2 -c exposure_absolute=5
+
 sudo -u pi LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0 python3 /home/pi/Vision2020/AngleTracker2020_pi_v2_multiCamera.py
